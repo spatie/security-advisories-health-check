4dbb6baf2d11621be51ada1fcd11a5a0473a7221
Freek Van der Herten <freek@spatie.be>
2022-08-03 23:23:20 +0200

    wip

--- src/SecurityAdvisoriesCheck.php
+++ src/SecurityAdvisoriesCheck.php
@@ -4,6 +4,7 @@ namespace Spatie\SecurityAdvisoriesHealthCheck;
 
 use Composer\InstalledVersions;
 use GuzzleHttp\Client;
+use Illuminate\Support\Collection;
 use Spatie\Health\Checks\Check;
 use Spatie\Health\Checks\Result;
 use Spatie\Packagist\PackagistClient;
@@ -15,30 +16,43 @@ class SecurityAdvisoriesCheck extends Check
     {
         $packages = $this->getInstalledPackages();
 
-        $advisories = $this
-            ->getPackagist()
-            ->getAdvisoriesAffectingVersions($packages);
+        $advisories = $this->getAdvisories($packages);
 
-        if (count($advisories) === 0) {
+        if ($advisories->isEmpty()) {
             return Result::make('No security vulnerability advisories found');
         }
 
-        Result::make();
+        $packageNames = $advisories->keys()
+            ->map(fn(string $packageName) => "`{$packageName}`")
+            ->join("'", ' and ');
 
-        dd($advisories);
+        return Result::make()
+            ->meta($advisories->toArray())
+            ->failed("Security advisories found for {$packageNames}");
     }
 
     /**
-     * @return array<string, string>
+     * @return Collection<string, string>
      */
-    protected function getInstalledPackages(): array
+    protected function getInstalledPackages(): Collection
     {
         return collect(InstalledVersions::getAllRawData()[0]['versions'])
             ->filter(fn(array $packageProperties) => isset($packageProperties['version']))
             ->mapWithKeys(function (array $packageProperties, string $package) {
                 return [$package => $packageProperties['version']];
-            })
-            ->toArray();
+            });
+    }
+
+    /**
+     * @return Collection<SecurityAdvisory>
+     */
+    protected function getAdvisories(Collection $packages): Collection
+    {
+        $advisories = $this
+            ->getPackagist()
+            ->getAdvisoriesAffectingVersions($packages->toArray());
+
+        return collect($advisories);
     }
 
     protected function getPackagist(): PackagistClient
--- tests/SecurityAdvisoriesCheckTest.php
+++ tests/SecurityAdvisoriesCheckTest.php
@@ -5,5 +5,13 @@ use Spatie\SecurityAdvisoriesHealthCheck\SecurityAdvisoriesCheck;
 it('can get security advisories', function () {
     $check = new SecurityAdvisoriesCheck();
 
-    dd($check->run());
+    $result = $check->run();
+
+    if ($result->ok()) {
+        expect($result->meta)->toBeEmpty();
+    }
+
+    if (! $result->ok()) {
+        expect(count($result->meta))->toBeGreaterThan(0);
+    }
 });
